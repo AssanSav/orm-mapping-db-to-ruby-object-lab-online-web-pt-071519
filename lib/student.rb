cffe0fbfb7dbe0c73a2d7e7c6b09baa5c242f5c6
assane savadogo <assane.savadogo81@gmail.com>
2019-10-01 20:14:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,7 +52,7 @@ class Student
   def self.find_by_name(name)
     sql = <<-SQL 
     SELECT * FROM students WHERE name = ? 
-    LIMIT !
+    LIMIT 1
     SQL
     DB[:conn].execute(sql, name).map do |row|
       Student.new_from_db(row)
