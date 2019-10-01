3138d119f29727f5d36939f18fa74192c7d01532
assane savadogo <assane.savadogo81@gmail.com>
2019-10-01 20:49:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -79,4 +79,13 @@ class Student
       Student.new_from_db(row)
     end
   end
+  
+  def self.first_X_students_in_grade_10
+    sql = <<-SQL
+    SELECT * FROM students WHERE garde = 10 
+    ORDER BY id ASC 
+    SQL
+    DB[:conn].execute(sql)
+    binding.pry
+  end
 end
