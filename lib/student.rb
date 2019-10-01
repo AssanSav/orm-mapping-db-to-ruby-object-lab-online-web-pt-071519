c3d39e3a8528ebed83643eef96b5982d3547055b
assane savadogo <assane.savadogo81@gmail.com>
2019-10-01 20:23:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,6 +18,7 @@ class Student
       binding.pry
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    end
   end
 
   def self.find_by_name(name)
