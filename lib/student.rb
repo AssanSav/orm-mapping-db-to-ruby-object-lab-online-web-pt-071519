8eeefa8349fba86826c8138ea8cbc7d6ceae4f49
assane savadogo <assane.savadogo81@gmail.com>
2019-10-01 20:57:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -82,8 +82,7 @@ class Student
   
   def self.first_student_in_grade_10
     sql = <<-SQL
-    SELECT * FROM students WHERE grade = 10 
-    ORDER BY students.id ASC LIMIT 1
+    SELECT * FROM students WHERE grade = 10 ASC LIMIT 1
     SQL
     DB[:conn].execute(sql)
     #binding.pry
